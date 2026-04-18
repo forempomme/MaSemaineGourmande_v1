35951fb54e118fba0fb02cbb6ce27518eef00acf
forempomme <duret.julienmic@gmail.com>
2026-04-18 16:33:29 +0200

    Last Sync: 2026-04-18 16:33 (Mobile)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,25 @@
 pluginManagement {
     repositories {
-        google { content { includeGroupByRegex("com\\.android.*"); includeGroupByRegex("com\\.google.*"); includeGroupByRegex("androidx.*") } }
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") { from(files("gradle/libs.versions.toml")) }
-    }
+    // ← bloc versionCatalogs supprimé : Gradle 8 charge
+    //   gradle/libs.versions.toml automatiquement
 }
 
 rootProject.name = "MaSemaineGourmande"
